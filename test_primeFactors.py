7e6be1ccb3557253a90aa9c5baa60353f72086cb
Dave Man <dmn001@gmail.com>
2013-05-29 20:11:46 +0100

    second test

--- test_primeFactors.py
+++ test_primeFactors.py
@@ -6,8 +6,11 @@ class primeFactorsTest(unittest.TestCase):
    def setUp(self):
       self.p = primeFactors()
 
-   def test_generate_returns_list(self):
+   def test_testOne(self):
       self.assertEqual([], self.p.generate(1))
+
+   def test_testTwo(self):
+      self.assertEqual([2], self.p.generate(2))
 
 if __name__ == "__main__":
 	unittest.main()
