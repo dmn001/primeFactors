d21299a56154b1b1ece5f6338131be1190ed298d
Dave Man <dmn001@gmail.com>
2013-05-29 20:17:13 +0100

    test Four fail

--- test_primeFactors.py
+++ test_primeFactors.py
@@ -3,17 +3,20 @@ from primeFactors import primeFactors
  
 class primeFactorsTest(unittest.TestCase):
 
-   def setUp(self):
-      self.p = primeFactors()
+	def setUp(self):
+		self.p = primeFactors()
 
-   def test_testOne(self):
-      self.assertEqual([], self.p.generate(1))
+	def test_One(self):
+		self.assertEqual([], self.p.generate(1))
 
-   def test_testTwo(self):
-      self.assertEqual([2], self.p.generate(2))
+	def test_Two(self):
+		self.assertEqual([2], self.p.generate(2))
 
-   def test_testThree(self):
-      self.assertEqual([3], self.p.generate(3))
+	def test_Three(self):
+		self.assertEqual([3], self.p.generate(3))
+
+	def test_Four(self):
+		self.assertEqual([2,2], self.p.generate(4))
 
 if __name__ == "__main__":
 	unittest.main()
